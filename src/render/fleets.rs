d6dee2a9da74f3c453ee91d742b8286faca2e4fc
souptheloop <95488694+souptheloop@users.noreply.github.com>
2022-05-05 07:35:11 +0100

    QoL features
    - Display the date as ‘TODAY’ if the fleet is today
    - Displayed TBD when there is no assigned FC
    - Trim [RESERVED] from fleet names

--- src/render/fleets.rs
+++ src/render/fleets.rs
@@ -1,3 +1,4 @@
+use chrono::{Datelike, Utc};
 use chrono::format::{DelayedFormat, StrftimeItems};
 use crate::models::fleet::{Fleet, FleetType};
 
@@ -31,25 +32,42 @@ pub fn render_motd(fleets: &Vec<Fleet>) -> String {
 
 
 fn render_fleet(fleet: &Fleet) -> String {
-    let time = fleet.start.format("%d/%m %H:%M");
     let color = match fleet.fleet_type {
         FleetType::HS => Colors::HS,
         FleetType::LS => Colors::LS,
         FleetType::NS => Colors::NS,
-        FleetType::COVOPS => Colors:: COVOPS,
+        FleetType::COVOPS => Colors::COVOPS,
         FleetType::EVENT => Colors::EVENT,
         FleetType::TRAINING => Colors::TRAINING,
     };
-    return format_fleet(color, time, fleet.url.as_str(), fleet.name.as_str(), fleet.fc.as_str(), fleet.formup.as_str());
+    let fc = match fleet.fc.as_str() {
+        "" => "TBD",
+        s => s,
+    };
+    let name = fleet.name.replace("[RESERVED]", "");
+    let date = render_date(fleet);
+    return format_fleet(color, date.as_str(), fleet.url.as_str(), name.trim(), fc, fleet.formup.as_str());
 }
 
-fn format_fleet(color: &str, time: DelayedFormat<StrftimeItems>, url: &str, name: &str, fc: &str, location: &str) -> String {
+fn render_date(fleet: &Fleet) -> String {
+    let today = Utc::now();
+    let is_today = today.ordinal() == fleet.start.ordinal() && today.year() == fleet.start.year();
+    return if is_today {
+        format!("TODAY {}", fleet.start.format("%H:%M"))
+    } else {
+        format!("{}", fleet.start.format("%d/%m %H:%M"))
+    };
+
+}
+
+
+fn format_fleet(color: &str, time: &str, url: &str, name: &str, fc: &str, location: &str) -> String {
     format!(r###"<font size="12" color="{color}"><b>{time} <a href="{url}">{name}</a> w/{fc} @{location}</b></font>"###, color = color, time = time, url = url, name = name, fc = fc, location = location)
 }
 
 #[cfg(test)]
 mod tests {
-    use chrono::{TimeZone, Utc};
+    use chrono::{DateTime, Timelike, TimeZone, Utc};
     use rocket::form::validate::Contains;
     use crate::models::fleet::{Fleet, FleetType};
 
@@ -58,10 +76,10 @@ mod tests {
     #[test]
     fn renders_first_three_in_date_order() {
         let fleets = &vec![
-            fleet("fleet 1", "FC 1", "Jita", "2022-04-11 18:00:00.000000", "", FleetType::HS),
-            fleet("fleet 2", "FC 2", "Jita", "2022-04-11 19:00:00.000000", "", FleetType::HS),
-            fleet("fleet 3", "FC 3", "Jita", "2022-05-20 18:00:00.000000", "", FleetType::HS),
-            fleet("fleet 4", "FC 4", "Jita", "2022-04-14 18:00:00.000000", "", FleetType::HS)];
+            fleet("fleet 1", "FC 1", "Jita", date("2022-04-11 18:00:00.000000"), "", FleetType::HS),
+            fleet("fleet 2", "FC 2", "Jita", date("2022-04-11 19:00:00.000000"), "", FleetType::HS),
+            fleet("fleet 3", "FC 3", "Jita", date("2022-05-20 18:00:00.000000"), "", FleetType::HS),
+            fleet("fleet 4", "FC 4", "Jita", date("2022-04-14 18:00:00.000000"), "", FleetType::HS)];
 
         let result = render::fleets::render_motd(fleets);
         assert_eq!(true, result.contains("fleet 1"));
@@ -72,7 +90,7 @@ mod tests {
     #[test]
     fn renders_when_single_fleet() {
         let fleets = &vec![
-            fleet("fleet 1", "FC 1", "Jita", "2022-04-11 18:00:00.000000", "", FleetType::HS)];
+            fleet("fleet 1", "FC 1", "Jita", date("2022-04-11 18:00:00.000000"), "", FleetType::HS)];
 
         let result = render::fleets::render_motd(fleets);
 
@@ -91,7 +109,7 @@ mod tests {
     #[test]
     fn renders_hs_html() {
         let result = render::fleets::render_motd(&vec![
-            fleet("A Fleet", "A FC", "Jita", "2022-01-02 18:00:00.000000", "http://www.spectre-fleet.space", FleetType::HS)]);
+            fleet("A Fleet", "A FC", "Jita", date("2022-01-02 18:00:00.000000"), "http://www.spectre-fleet.space", FleetType::HS)]);
 
         assert_eq!(result, r###"<font size="12" color="#ff00ff00"><b>02/01 18:00 <a href="http://www.spectre-fleet.space">A Fleet</a> w/A FC @Jita</b></font>"###);
     }
@@ -99,19 +117,48 @@ mod tests {
     #[test]
     fn renders_ls_html() {
         let result = render::fleets::render_motd(&vec![
-            fleet("A Fleet", "A FC", "Jita", "2022-01-02 18:00:00.000000", "http://www.spectre-fleet.space", FleetType::LS)]);
+            fleet("A Fleet", "A FC", "Jita", date("2022-01-02 18:00:00.000000"), "http://www.spectre-fleet.space", FleetType::LS)]);
 
         assert_eq!(result, r###"<font size="12" color="#ffffff00"><b>02/01 18:00 <a href="http://www.spectre-fleet.space">A Fleet</a> w/A FC @Jita</b></font>"###);
     }
 
-    fn fleet(name: &str, fc: &str, form_up: &str, start: &str, url: &str, fleet_type: FleetType) -> Fleet {
+    #[test]
+    fn renders_tbd_when_no_fc() {
+        let result = render::fleets::render_motd(&vec![
+            fleet("A Fleet", "", "Jita", date("2022-01-02 18:00:00.000000"), "http://www.spectre-fleet.space", FleetType::LS)]);
+
+        assert_eq!(result, r###"<font size="12" color="#ffffff00"><b>02/01 18:00 <a href="http://www.spectre-fleet.space">A Fleet</a> w/TBD @Jita</b></font>"###);
+    }
+
+    #[test]
+    fn removes_reserved_from_fleet_name() {
+        let result = render::fleets::render_motd(&vec![
+            fleet("A Fleet [RESERVED]", "A FC", "Jita", date("2022-01-02 18:00:00.000000"), "http://www.spectre-fleet.space", FleetType::LS)]);
+
+        assert_eq!(result, r###"<font size="12" color="#ffffff00"><b>02/01 18:00 <a href="http://www.spectre-fleet.space">A Fleet</a> w/A FC @Jita</b></font>"###);
+    }
+
+    #[test]
+    fn renders_date_with_today_when_fleet_today() {
+        let today = Utc::now().with_hour(18).unwrap().with_minute(00).unwrap();
+        let result = render::fleets::render_motd(&vec![
+            fleet("A Fleet", "A FC", "Jita", today, "http://www.spectre-fleet.space", FleetType::LS)]);
+
+        assert_eq!(result, r###"<font size="12" color="#ffffff00"><b>TODAY 18:00 <a href="http://www.spectre-fleet.space">A Fleet</a> w/A FC @Jita</b></font>"###);
+    }
+
+    fn fleet(name: &str, fc: &str, form_up: &str, start: DateTime<Utc>, url: &str, fleet_type: FleetType) -> Fleet {
         render::fleets::Fleet {
             name: name.to_string(),
             fc: fc.to_string(),
             formup: form_up.to_string(),
             url: url.to_string(),
-            start: Utc.datetime_from_str(start, "%F %H:%M:%S.%f").unwrap(),
+            start: start,
             fleet_type: fleet_type,
         }
+    }
+
+    fn date(d: &str) -> DateTime<Utc> {
+        return Utc.datetime_from_str(d, "%F %H:%M:%S.%f").unwrap();
     }
 }
